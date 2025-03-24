600c807c92765f8ff608bf94f01e61904d48cc79
IsNoobDoog <50238909+IsNoobDoog@users.noreply.github.com>
2025-03-24 15:44:50 +0000

    First Commit

---
+++ src/js/components/FooterLandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; 
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3 fixed-bottom">
+      © 2024 - Todos los derechos reservados
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/js/components/JumbotronProduct.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="custom-jumbotron">
+      <div className="container">
+        <h1 className="display-4">Bienvenido a mi página</h1>
+        <p className="lead">Esto es un jumbotron personalizado en React.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
